96bc46cc8f5f85d3b2939d2fb394cc61726cec51
takehiro1111 <t.soccer.0916@gmail.com>
2025-05-14 23:03:19 +0900

    add:テーブルのスキーマ定義を修正

--- todo-project/sql/seeds.sql
+++ todo-project/sql/seeds.sql
@@ -1,13 +1,19 @@
 BEGIN;
 
-DELETE FROM todos;
-DELETE FROM users;
+-- 外部キー制約があるままだとusersテーブルを削除できないため。
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE todos; 
+TRUNCATE TABLE users;
+SET FOREIGN_KEY_CHECKS = 1;
 
 -- users
+-- hashed_password_dummy_1(sha256)
+-- hashed_password_dummy_2(sha512)
+-- hashed_password_dummy_3(MD5)
 INSERT INTO users (name, email, password_hash) VALUES
-('山田太郎', 'yamada@example.com', 'hashed_password_dummy_1'),
-('佐藤花子', 'sato@example.com', 'hashed_password_dummy_2'),
-('高橋ジョージ', 'takahashi@example.com', 'hashed_password_dummy_3');
+('山田太郎', 'yamada@example.com', '20262b45538ad0741cba091296236bd2c804e698870ee8472aac846f0113f111'),
+('佐藤花子', 'sato@example.com', '6881f0556de3db6050ec93387ba6337c13cbb9eb8b9bab122d37e1153cec57dd29a2bfc1413daab8e13489b3e7a2f73ec740bb3da8605514e89db28808501d9f'),
+('高橋ジョージ', 'takahashi@example.com', '265529a61935301d219ea7ce1edda400');
 
 
 -- todos

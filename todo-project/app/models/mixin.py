ec0b39bf4e296ab86f8dc2b6cf0d473755bd9eef
takehiro1111 <t.soccer.0916@gmail.com>
2025-06-01 22:52:46 +0900

    update:mixinの処理をmodelのメソッドで直接定義するよう修正

--- todo-project/app/models/mixin.py
+++ todo-project/app/models/mixin.py
@@ -17,25 +17,3 @@ class DeleteMixin:
   def soft_delete(self, tz="Asia/Tokyo"):
     """論理削除（deleted_atを現在時刻で更新）/ 呼び出し元でcommitを行う。"""
     self.deleted_at = datetime.now(ZoneInfo(tz))
-
-class TodosBaseModeMixin:
-  def _base_query_by_todo_id(self, session: Session, todo_id):
-    """todo_idでfilterする場合"""
-    from app.models.create_tables import Todos
-    return Todos.active_query(session).filter(Todos.id == todo_id)
-  
-  def _base_query_by_user_id(self, session: Session, user_id):
-    """user_idでfilterする場合"""
-    from app.models.create_tables import Todos
-    return Todos.active_query(session).filter(Todos.user_id == user_id)
-
-class UsersBaseModeMixin:
-  def _base_query_by_user_id(self, session: Session, id):
-    """主キーのidでユーザーをfilterする場合"""
-    from app.models.create_tables import Users
-    return Users.active_query(session).filter(Users.id == id)
-  
-  def _base_query_by_email(self, session: Session, email):
-    """メールアドレスでユーザーをfilterする場合"""
-    from app.models.create_tables import Users
-    return Users.active_query(session).filter(Users.email == email)
--- todo-project/app/models/todos.py
+++ todo-project/app/models/todos.py
@@ -1,16 +1,23 @@
 from app.models.session import SessionLocal
 from app.models.create_tables import Todos
-from app.models.mixin import TodosBaseModeMixin
 from utils.messages import ERROR_MESSAGES
 
-class Todo(TodosBaseModeMixin):
+class Todo:
   def __init__(self):
     self.session = SessionLocal()
+    
+  def _base_query_by_todo_id(self, todo_id):
+    """todo_idでfilterする場合"""
+    return Todos.active_query(self.session).filter(Todos.id == todo_id)
+  
+  def _base_query_by_user_id(self, user_id):
+    """user_idでfilterする場合"""
+    return Todos.active_query(self.session).filter(Todos.user_id == user_id)
     
   def find_by_user(self, user_id):
     """ログイン時のTodoの取得"""
     return (
-      self._base_query_by_user_id(self.session, user_id).all()
+      self._base_query_by_user_id(user_id).all()
     )
     
   def insert_todo(self, title, body, user_id):
@@ -22,11 +29,11 @@ class Todo(TodosBaseModeMixin):
     
   def select_todo_by_id(self, todo_id: int):
     """IDが一致するTodoの取得"""
-    return self._base_query_by_todo_id(self.session, todo_id).first()
+    return self._base_query_by_todo_id(todo_id).first()
     
   def update_todo_by_id(self,title, body, todo_id: int):
     """IDが一致するTodoの更新"""
-    todo_to_update = self._base_query_by_todo_id(self.session, todo_id).first()
+    todo_to_update = self._base_query_by_todo_id(todo_id).first()
         
     if not todo_to_update:
         raise ValueError(ERROR_MESSAGES["user_model"]["TODO_ID_NOT_FOUND"].format(todo_id))
@@ -38,7 +45,7 @@ class Todo(TodosBaseModeMixin):
     
   def soft_delete_todo_by_id(self, todo_id: int):
     """IDが一致するTodoの論理削除"""    
-    todo_to_delete = self._base_query_by_todo_id(self.session, todo_id).first()
+    todo_to_delete = self._base_query_by_todo_id(todo_id).first()
     
     if not todo_to_delete:
         raise ValueError(ERROR_MESSAGES["user_model"]["TODO_ID_NOT_FOUND"].format(todo_id))
